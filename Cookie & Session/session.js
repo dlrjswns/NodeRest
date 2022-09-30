d9d24b66d45fb33fcb45e40afbf9fb0894eaf72b
dlrjswns <leekyunjun@gmail.com>
2022-09-30 10:15:02 +0900

    session.js parseCookies 추가

--- Cookie & Session/session.js
+++ Cookie & Session/session.js
@@ -4,6 +4,16 @@ const fs= require('fs').promises;
 const url = require('url');
 const qs = require('querystring');
 
+const parseCookies = (cookie = '') => 
+    cookie
+        .split(';')
+        .map(v => v.split('='))
+        .reduce((acc, [k, v]) => {
+            acc[k.trim()] = decodeURIComponent(v);
+            return acc;
+        }, {});
+
+
 http.createServer(async (req, res) => {
     const cookies = parseCookies(req.headers.cookie);
     if (req.url.startsWith('/login')) {
