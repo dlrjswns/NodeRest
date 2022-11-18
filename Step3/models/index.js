419794ea494fba8f261cfbd0afafe3210bdd8ae9
dlrjswns <leekyunjun@gmail.com>
2022-11-18 10:04:50 +0900

    MySQL 연동 Step3

---
+++ Step3/models/index.js
@@ -0,0 +1,27 @@
+const Sequelize = require('sequelize');
+const User = require('./user');
+const Comment = require('./comment');
+
+const env = process.env.NODE_ENV || 'development';
+const config = require('../config/config')[env]; // development의 데이터베이스 아이디 비번과 같은 정보를 가져오겠군
+const db = {};
+
+const sequelize = new Sequelize(
+    config.database, config.username, config.password, config
+);
+
+db.sequelize = sequelize;
+db.Sequelize = Sequelize;
+
+db.User = User;
+db.Comment = Comment;
+
+User.init(sequelize);
+Comment.init(sequelize);
+
+User.associate(db);
+Comment.associate(db);
+
+module.exports = db;
+
+
